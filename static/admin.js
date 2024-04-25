5d9b7520668c8a978beb44e8e2d8353f6ef22a18
Ren Tatsumoto <tatsu@autistici.org>
2024-04-25 16:56:36 +0300

    change comment style

--- static/admin.js
+++ static/admin.js
@@ -1,29 +1,4 @@
-/**
- *
- * @licstart  The following is the entire license notice for the
- *  JavaScript code in this page.
- *
- * Copyright (C) 2024  Rapptz <rapptz at gmail dot com>
- *
- * The JavaScript code in this page is free software: you can
- * redistribute it and/or modify it under the terms of the GNU Affero
- * General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- *
- * The code is distributed WITHOUT ANY WARRANTY; without even the
- * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- *
- * As additional permission under GNU Affero General Public License
- * section 7, you may distribute non-source (e.g., minimized or compacted)
- * forms of that code without the copy of the GNU Affero General Public
- * License normally required by section 4, provided you include this
- * license notice and a URL through which recipients can access the
- * Corresponding Source.
- *
- * @licend  The above is the entire license notice
- *  for the JavaScript code in this page.
- */
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-v3-or-Later
 
 const logSelect = document.getElementById('log-select');
 // Statistics
@@ -244,3 +219,4 @@ logSelect.addEventListener('change', async () => {
 });
 
 getLogs(logSelect.value).then(updateGraphs);
+// @license-end
--- static/admin_trash.js
+++ static/admin_trash.js
@@ -1,3 +1,5 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-v3-or-Later
+
 async function processTrashRequest(action) {
   let files = getSelectedFiles().map(e => e.textContent);
   if (files.length === 0) {
@@ -30,3 +32,5 @@ async function processTrashRequest(action) {
 
 document.getElementById('trash-files').addEventListener('click', () => processTrashRequest('delete'));
 document.getElementById('restore-files').addEventListener('click', () => processTrashRequest('restore'));
+
+// @license-end
