363a1a2bf53b2c7fd84b7ea85379d44951c7268d
Rapptz <rapptz@gmail.com>
2024-05-16 14:07:28 -0400

    Remove mutation observer for API docs due to upstream change

--- static/api.js
+++ static/api.js
@@ -1,13 +1,1 @@
 /* This file is licensed under AGPL-3.0 */
-const detectChanges = (mutationList, observer) => {
-  const el = document.getElementById('security-scheme-Authorization');
-  if (el === null) {
-    return;
-  }
-  if (el.type === 'password') {
-    el.type = 'text';
-  }
-}
-
-const observer = new MutationObserver(detectChanges);
-observer.observe(document.body, {childList: true, subtree: true})
