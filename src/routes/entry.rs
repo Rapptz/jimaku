bdf8fd1ac54f203857e7b263595dc22d36868f84
Rapptz <rapptz@gmail.com>
2026-07-16 05:38:41 -0400

    Sort entries by file name in the API

--- src/routes/entry.rs
+++ src/routes/entry.rs
@@ -86,6 +86,8 @@ pub(crate) fn get_file_entries(entry_id: i64, path: &std::path::Path) -> std::io
             last_modified,
         });
     }
+
+    entries.sort_by(|a, b| a.name.cmp(&b.name));
     Ok(entries)
 }
 
